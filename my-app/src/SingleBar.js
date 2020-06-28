be45dabd73467641e5771b37b76a586eeb5f81e3
Andrea Bogazzi <abogazzi@shutterstock.com>
2020-06-28 23:52:12 +0200

    a test

--- my-app/src/SingleBar.js
+++ my-app/src/SingleBar.js
@@ -3,11 +3,13 @@ import React from 'react';
 const SingleBar = ({ animations }) => {
   return <div className="animationBar">
   {animations.map(animation =>
+    <div className="animationContainer">
     <div key={`${animation.property}-${animation.value}`} className="animation" style={{
       width: animation.duration / 10,
-      left: animation.offset / 10,
+      marginLeft: animation.offset / 10,
     }}>
       {animation.property}: {animation.value}
+    </div>
     </div>
   )}
   </div>
